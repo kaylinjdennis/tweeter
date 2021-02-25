a9695a8e9449167fcd81056cdcac075120e0fa98
Kaylin Dennis <kaylinjdennis@gmail.com>
2021-02-25 16:19:30 -0700

    Implement jquery get and post requests

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,9 +5,22 @@
 */
 
 $(document).ready(function() {
-  renderTweets(data);
-});
+  const $loadTweets = function() {
+    $.ajax('/tweets', {method: 'GET'})
+      .then(function(tweets) {
+        renderTweets(tweets);
+      });
+  };
 
+  $('#tweet-form').submit(function(event) {
+    event.preventDefault();
+    $.ajax({ method: "POST", url: "/tweets", data: $(this).serialize()})
+      .done(function() {
+        $loadTweets();
+      });
+  });
+  $loadTweets();
+});
 
 const createTweetElement = (tweet) => {
   const $tweet = $(`
@@ -34,33 +47,14 @@ const renderTweets = (tweets) => {
   // loops through tweets
   // calls createTweetElement for each tweet
   // takes return value and appends it to the tweets container
-  for (let tweet of tweets) {
-    const $tweet = createTweetElement(tweet);
+  const numOfInitialTweets = 2;
+  if (tweets.length > numOfInitialTweets) {
+    const $tweet = createTweetElement(tweets[tweets.length - 1]);
     $('#tweets-container').append($tweet);
+  } else {
+    for (let tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $('#tweets-container').append($tweet);
+    }
   }
 };
-
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
