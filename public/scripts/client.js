6cbf5cb67a8d856163be53b61e20dbe348538766
Kaylin Dennis <kaylinjdennis@gmail.com>
2021-02-25 19:32:22 -0700

    change alerts for validation errors to html element

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,6 +5,7 @@
 */
 
 $(document).ready(function() {
+  $('#form-error').hide();
   const $loadTweets = function() {
     $.ajax('/tweets', {method: 'GET'})
       .then(function(tweets) {
@@ -14,11 +15,16 @@ $(document).ready(function() {
 
   $('#tweet-form').submit(function(event) {
     event.preventDefault();
+    $('#form-error').hide();
+    $('#min-error').hide();
+    $('#max-error').hide();
     const counter = document.getElementsByClassName('counter').counter.innerHTML;
     if ($(this).serialize().length === 5) {
-      alert('Error: no tweet content');
+      $('#form-error').show();
+      $('#min-error').show();
     } else if (counter < 0) {
-      alert('Error: tweet content exceeds 140 characters');
+      $('#form-error').show();
+      $('#max-error').show();
     } else {
       $.ajax({ method: "POST", url: "/tweets", data: $(this).serialize()})
         .done(function() {
@@ -40,7 +46,7 @@ const createTweetElement = (tweet) => {
 				<span>${tweet.user.handle}</span>
 			</header>
 			<div class="tweet-body">
-				<span>${tweet.content.text}</span>
+				<span>${escape(tweet.content.text)}</span>
 			</div>
 			<footer>
 				<p>${(Math.floor((Date.now() - tweet.created_at) / 86400000))} days ago</p>
@@ -50,11 +56,17 @@ const createTweetElement = (tweet) => {
   return $tweet;
 };
 
+const escape =  function(str) {
+  let div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
+
 const renderTweets = (tweets) => {
   // loops through tweets
   // calls createTweetElement for each tweet
   // takes return value and appends it to the tweets container
-  const numOfInitialTweets = 2;
+  const numOfInitialTweets = $('#tweets-container').children.length;
   if (tweets.length > numOfInitialTweets) {
     const $tweet = createTweetElement(tweets[tweets.length - 1]);
     $('#tweets-container').append($tweet);
